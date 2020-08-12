a47ec78686600f1e4d65ffcbde71e1900b169bdc
Develop <2342342342342@234234232322.com>
2020-08-11 18:07:02 -0700

    Added Optional: Add a Settings Screen to the Tip Calculator

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -19,6 +19,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+        let defaults = UserDefaults.standard
+        let defaulttipIndex = defaults.integer(forKey: "defaulttipIndexInt")
+         
+         tipControl.selectedSegmentIndex = defaulttipIndex
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
     }
 
     @IBAction func onTap(_ sender: Any) {
